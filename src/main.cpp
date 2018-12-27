3cafe3ab377df02b9b41b39b578472cf5ff5e175
Pawel <CiemnyPawel>
2018-12-27 12:53:25 +0100

    Change LF to CRLF

--- src/main.cpp
+++ src/main.cpp
@@ -12,16 +12,16 @@ using Map = aisdi::Map<K, V>;
 
 void perfomTest()
 {
-  Map<int, std::string> map;
-  map[1] = "TODO";
+        Map<int, std::string> map;
+        map[1] = "TODO";
 }
 
 } // namespace
 
 int main(int argc, char** argv)
 {
-  const std::size_t repeatCount = argc > 1 ? std::atoll(argv[1]) : 10000;
-  for (std::size_t i = 0; i < repeatCount; ++i)
-    perfomTest();
-  return 0;
+        const std::size_t repeatCount = argc > 1 ? std::atoll(argv[1]) : 10000;
+        for (std::size_t i = 0; i < repeatCount; ++i)
+                perfomTest();
+        return 0;
 }
